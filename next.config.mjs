e3d9b0a2e3a3a3bafcce0c1de022e7cb857297ba
Arsen Aleksanyan <arsen.aleksanyan.1994@gmail.com>
2024-09-23 18:15:08 +0400

    Add en and ru language support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  i18n: {
+    locales: ['en', 'ru'],
+    defaultLocale: 'en',
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
